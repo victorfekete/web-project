a97a7a7e7007a13f783f69cfb2384db115228127
victorfekete <victorfekete10@gmail.com>
2025-11-13 12:52:46 +0200

    02:customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,40 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+import { Checkbox } from "@/components/ui/checkbox";
 
 export default function Home() {
   return (
+    <div className="p-4">
+    <div className="flex flex-col gap-y-4">
+
     <div>
-      <Button>
-        Hello world
+      <Button variant="elevated">
+        Button
       </Button>
 
     </div>
+
+    <div>
+      <Input placeholder="I am an input" />
+    </div>
+
+    <div>
+      <Progress value={50} />
+    </div>
+
+    <div>
+      <Textarea placeholder="I am a text area" />
+    </div>
+
+    <div>
+      <Checkbox />
+    </div>
+
+    </div>
+    </div>
   );
 };
+
+
