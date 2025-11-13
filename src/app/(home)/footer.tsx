e0cae7ec5fc184c5f25057900638c7c5afc516bc
victorfekete <victorfekete10@gmail.com>
2025-11-13 23:59:35 +0200

    03: layout

---
+++ src/app/(home)/footer.tsx
@@ -0,0 +1,11 @@
+export const Footer = () => {
+    return( 
+        <footer className="flex border-t justify-between font-medium p-6">
+            <div className="flex items-center gap-2">
+                <p>Funroad, Inc.
+                </p>
+            </div>
+        </footer>
+
+    )
+}
