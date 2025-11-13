736c4cb9b6bd79d86869c8639be9ce820eb9eb69
victorfekete <victorfekete10@gmail.com>
2025-11-14 00:04:08 +0200

    03: layout fix

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -52,7 +52,7 @@ const navbarItems = [
 
 export const Navbar = () => {
  const pathname = usePathname();
- const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+ const [isSidebarOpen, setIsSidebarOpen] = useState(false);
     return (
         <nav className="h-20 flex border-b justify-between font-medium bg-white">
             <Link href="/" className="pl-6 flex items-center">
